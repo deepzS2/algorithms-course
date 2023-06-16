4164f676b83d4693105947840e03eb3e4bd22b5a
deepzS2 <alanbackman10@hotmail.com>
2023-06-16 15:39:49 -0300

    feat: reverse integer exercise resolution

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const numberString = n.toString().replace("-", "");
+  const numberSign = n >= 0 ? 1 : -1;
+
+  const reversedNumberString = numberString
+    .split("")
+    .reduce((reversed, char) => char + reversed, "");
+
+  const reversedInt = parseInt(reversedNumberString);
+
+  return reversedInt * numberSign;
+}
 
 module.exports = reverseInt;
