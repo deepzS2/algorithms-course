52f51bfa1e7c0c62385e8fb73d8b1818fcdbd245
deepzS2 <alanbackman10@hotmail.com>
2023-06-17 13:14:51 -0300

    feat: anagrams exercise resolution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,37 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const NON_WORD_CHARACTER_REGEX = /[^\w]/g;
+
+const createCharsMap = (string) => {
+  const charsMap = new Map();
+
+  for (const char of string
+    .replace(NON_WORD_CHARACTER_REGEX, "")
+    .toLowerCase()) {
+    charsMap.set(char, charsMap.has(char) ? charsMap.get(char) + 1 : 1);
+  }
+
+  return charsMap;
+};
+
+function anagrams(stringA, stringB) {
+  const charactersAMap = createCharsMap(stringA);
+  const charactersBMap = createCharsMap(stringB);
+
+  const isCharactersMapsSizeEquals =
+    charactersAMap.size === charactersBMap.size;
+
+  if (!isCharactersMapsSizeEquals) return false;
+
+  for (const char of charactersAMap.keys()) {
+    const charACount = charactersAMap.has(char) ? charactersAMap.get(char) : 0;
+    const charBCount = charactersBMap.has(char) ? charactersBMap.get(char) : 0;
+
+    if (charACount !== charBCount) return false;
+  }
+
+  return true;
+}
 
 module.exports = anagrams;
