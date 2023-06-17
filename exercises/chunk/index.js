2d4235cb682c1f4f2c395de3ac13478908c79e58
deepzS2 <alanbackman10@hotmail.com>
2023-06-17 00:45:02 -0300

    feat: chunk exercise resolution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const result = [];
+
+  for (let item of array) {
+    const lastSubArray = result[result.length - 1];
+    const isLastSubArrayNull = !lastSubArray;
+
+    if (isLastSubArrayNull || lastSubArray.length === size) {
+      result.push([item]);
+    } else {
+      lastSubArray.push(item);
+    }
+  }
+
+  return result;
+}
 
 module.exports = chunk;
