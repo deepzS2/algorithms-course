994152e78fd2e68fa01d00d4da2b10bcbe12b11d
deepzS2 <alanbackman10@hotmail.com>
2023-06-16 21:27:49 -0300

    feat: max character exercise resolution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charsCountMap = new Map();
+  let maxCharacter = { value: "", count: 0 };
+
+  for (const char of str) {
+    const previousCharCount = charsCountMap.has(char)
+      ? charsCountMap.get(char)
+      : 0;
+    const newCharCount = previousCharCount + 1;
+
+    if (newCharCount > maxCharacter.count) {
+      maxCharacter.count = newCharCount;
+      maxCharacter.value = char;
+    }
+
+    charsCountMap.set(char, newCharCount);
+  }
+
+  return maxCharacter.value;
+}
 
 module.exports = maxChar;
