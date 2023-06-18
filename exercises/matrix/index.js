dc192b898035c14edbaa16f869784be41c4db326
deepzS2 <alanbackman10@hotmail.com>
2023-06-18 08:06:40 -0300

    feat: matrix exercise resolution

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,60 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = Array.from(Array(n), () => new Array(n));
+  let counter = 1;
+
+  let startColumn = 0;
+  let endColumn = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    // Top left to top right
+    for (
+      let columnIndex = startColumn;
+      columnIndex <= endColumn;
+      columnIndex++
+    ) {
+      result[startRow][columnIndex] = counter;
+      counter++;
+    }
+
+    startRow++;
+
+    // Top right to bottom right
+    for (let rowIndex = startRow; rowIndex <= endRow; rowIndex++) {
+      result[rowIndex][endColumn] = counter;
+      counter++;
+    }
+
+    endColumn--;
+
+    // Bottom right to bottom left
+    for (
+      let columnIndex = endColumn;
+      columnIndex >= startColumn;
+      columnIndex--
+    ) {
+      result[endRow][columnIndex] = counter;
+      counter++;
+    }
+
+    endRow--;
+
+    // Bottom left to top left
+    for (let rowIndex = endRow; rowIndex >= startRow; rowIndex--) {
+      result[rowIndex][startColumn] = counter;
+      counter++;
+    }
+
+    startColumn++;
+  }
+
+  return result;
+}
+
+console.dir(matrix(3));
 
 module.exports = matrix;
