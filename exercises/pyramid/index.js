d18a37dd35278c4a6e4acc4a1a7f129dccacc8e9
deepzS2 <alanbackman10@hotmail.com>
2023-06-18 07:24:56 -0300

    feat: pyramid exercise resolution

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const amountOfColumns = n * 2 - 1;
+  const middle = Math.floor(amountOfColumns / 2);
+
+  for (let rowIndex = 0; rowIndex < n; rowIndex++) {
+    let str = "";
+
+    for (let columnIndex = 0; columnIndex < amountOfColumns; columnIndex++) {
+      const isInMiddleOrOnBoundsOfMiddle =
+        columnIndex === middle ||
+        columnIndex === middle + rowIndex ||
+        columnIndex === middle - rowIndex;
+      const isInLeftMiddle =
+        columnIndex < middle && columnIndex > middle - rowIndex;
+      const isInRightMiddle =
+        columnIndex > middle && columnIndex < middle + rowIndex;
+
+      if (isInMiddleOrOnBoundsOfMiddle || isInLeftMiddle || isInRightMiddle) {
+        str += "#";
+      } else {
+        str += " ";
+      }
+    }
+
+    console.log(str);
+  }
+}
+
+pyramid(3);
 
 module.exports = pyramid;
