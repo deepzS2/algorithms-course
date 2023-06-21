347c455392865e80bbbfa3a0fd0bbcfb937051a8
deepzS2 <alanbackman10@hotmail.com>
2023-06-20 23:44:25 -0300

    feat: binary search tree exercise resolution

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,50 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    const isLeftNode = data < this.data;
+
+    if (isLeftNode) {
+      if (!this.left) {
+        this.left = new Node(data);
+        return;
+      }
+
+      this.left.insert(data);
+    } else {
+      if (!this.right) {
+        this.right = new Node(data);
+        return;
+      }
+
+      this.right.insert(data);
+    }
+  }
+
+  contains(data) {
+    const isNodeEquals = data === this.data;
+
+    if (isNodeEquals) return this;
+
+    const isLeftNode = data < this.data;
+
+    if (isLeftNode) {
+      if (!this.left) return null;
+
+      return this.left.contains(data);
+    } else {
+      if (!this.right) return null;
+
+      return this.right.contains(data);
+    }
+  }
+}
 
 module.exports = Node;
