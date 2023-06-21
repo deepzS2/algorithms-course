fac1ffb574b927466f579f541f6506a7d249e934
deepzS2 <alanbackman10@hotmail.com>
2023-06-21 00:17:15 -0300

    feat: binary search tree validation exercise resolution

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,18 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  const data = node.data;
+
+  const isDataInInvalidRange =
+    (min !== null && data < min) || (max !== null && data > max);
+
+  let isNodeValid = !isDataInInvalidRange;
+
+  if (node.left && isNodeValid) isNodeValid = validate(node.left, min, data);
+  if (node.right && isNodeValid) isNodeValid = validate(node.right, data, max);
+
+  return isNodeValid;
+}
 
 module.exports = validate;
