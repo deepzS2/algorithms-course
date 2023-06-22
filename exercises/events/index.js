296985104d1bcef9e5a766c2f603ce1f24dd124a
deepzS2 <alanbackman10@hotmail.com>
2023-06-22 13:03:56 -0300

    feat: events exercise resolution

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,39 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  #eventsMap = new Map();
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.#eventsMap.has(eventName)) {
+      return this.#eventsMap.set(eventName, [callback]);
+    }
+
+    const callbacks = this.#eventsMap.get(eventName);
+    callbacks.push(callback);
+
+    return this.#eventsMap.set(eventName, callbacks);
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (!this.#eventsMap.has(eventName)) return;
+
+    const callbacks = this.#eventsMap.get(eventName);
+
+    for (const callback of callbacks) {
+      callback();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if (!this.#eventsMap.has(eventName)) return;
+
+    this.#eventsMap.delete(eventName);
+  }
 }
 
 module.exports = Events;
